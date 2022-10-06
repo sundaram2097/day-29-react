5a3165786ffcef87074e728643f253c5d98f2e84
sundaram2097 <meenakshisundaram2097@gmail.com>
2022-10-06 14:45:23 +0530

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import { Users } from "./Users";
+
+export const Home = () => {
+  return (
+    <>
+      <h1>UserList</h1>
+      {/* displaying Users in Home */}
+      <Users />  
+    </>
+  );
+};
